65473d40296040f4a824efca0f4f1a2aa115e6a8
Bob Andrews <mrdudz@users.noreply.github.com>
2023-08-25 18:49:40 +0200

    some cleanup, less annoying noise :)

--- src/test-avi.c
+++ src/test-avi.c
@@ -31,8 +31,10 @@
 
 #define VIDEO_FPS           50
 #define VIDEO_NUM_FRAMES    (VIDEO_FPS * 30)    // 30 seconds
+//#define VIDEO_NUM_FRAMES    1
 
 #define AUDIO_FREQ          48000
+#define AUDIO_CHANNELS      2
 
 #define AUDIO_SIZE          (AUDIO_FREQ / VIDEO_FPS)
 
@@ -48,44 +50,55 @@ static int frameno = 0;
 static zmvb_init_flags_t iflg = ZMBV_INIT_FLAG_NONE;
 
 static int complevel = -1;  /* compression level, -1 means default */
-static int no_zlib = 0;
+
 static int verbose = 0;
 
 static uint8_t cur_pal[PALETTE_SIZE];
 static uint8_t cur_screen[VIDEO_SIZE];
 
-static int16_t cur_audio[AUDIO_SIZE];
+static int16_t cur_audio[AUDIO_SIZE * AUDIO_CHANNELS];
 
 ////////////////////////////////////////////////////////////////////////////////
 static void generate_video_frame (int idx) {
-
     int x, y;
     if (verbose > 1) { printf("generate_video_frame: %d\n", idx); }
+#if 1
     for (y = 0; y < PALETTE_NUM_COLORS; ++y) {
         for (x = 0; x < (PALETTE_COLORS_BPP / 8); ++x) {
             cur_pal[(y * (PALETTE_COLORS_BPP / 8)) + x] = (y*(PALETTE_COLORS_BPP / 8)  + ((x*3) ^ idx)) & 0xff;
         }
     }
+#endif
+#if 1
     for (y = 0; y < VIDEO_HEIGHT; ++y) {
         for (x = 0; x < VIDEO_WIDTH; ++x) {
             cur_screen[(y * VIDEO_WIDTH) + x] = ((x*3) + idx * 7 ^ (y*1) + idx) & 0xff;
         }
     }
+#endif
+#if 1
     // progress bar at the top
     for (y = 0; y < 10; ++y) {
         for (x = 0; x < (idx % VIDEO_WIDTH); ++x) {
             cur_screen[(y * VIDEO_WIDTH) + x] = 0xff;
         }
     }
+#endif
+}
+
+static double smpcount;
+static double get_audio_sample(void)
+{
+    smpcount+=0.0050;
+    return sin(smpcount);
 }
 
 static void generate_audio_frame (int idx) {
-    float step;
     int x;
-    step = (idx * AUDIO_SIZE) / 0.01f;
-    for (x = 0; x < AUDIO_SIZE; ++x) {
-        cur_audio[x] = sin(step) * 32000.0f;
-        step += 0.01f;
+    for (x = 0; x < AUDIO_SIZE*4; x+=2) {
+        double smp = get_audio_sample() * 4000.0f;
+        cur_audio[x] = (int16_t)smp;
+        cur_audio[x+1] = (int16_t)smp;
     }
 }
 
@@ -129,14 +142,14 @@ static int do_encode_screens (zmbv_avi_t zavi) {
         if (verbose > 1) {
             printf("do_encode_screens: frame %d\n", frameno);
         }
-
+#if 1
         // encode video frame
         if (zmbv_encode_prepare_frame(zcodec, flags, fmt, cur_pal, buf, buf_size) < 0) {
             printf("FATAL: can't prepare frame for screen #%d\n", frameno);
             break;
         }
         for (int y = 0; y < VIDEO_HEIGHT; ++y) {
-            if (zmbv_encode_line(zcodec, cur_screen+y*VIDEO_WIDTH) < 0) {
+            if (zmbv_encode_line(zcodec, cur_screen + (y * VIDEO_WIDTH)) < 0) {
                 printf("FATAL: can't encode line #%d for screen #%d\n", y, frameno);
                 break;
             }
@@ -146,14 +159,15 @@ static int do_encode_screens (zmbv_avi_t zavi) {
             printf("FATAL: can't finish frame for screen #%d\n", frameno);
             break;
         }
-        // write avi chunks
-        if (zmbv_avi_write_chunk_audio(zavi, cur_audio, AUDIO_SIZE) < 0) {
-            printf("FATAL: can't write audio frame for screen #%d\n", frameno);
-            break;
-        }
         // write avi chunk
         if (zmbv_avi_write_chunk_video(zavi, buf, written) < 0) {
             printf("FATAL: can't write compressed frame for screen #%d\n", frameno);
+            break;
+        }
+#endif
+        // write avi chunks
+        if (zmbv_avi_write_chunk_audio(zavi, cur_audio, AUDIO_SIZE) < 0) {
+            printf("FATAL: can't write audio frame for screen #%d\n", frameno);
             break;
         }
     }
@@ -178,10 +192,11 @@ static void encode_screens_to_avi (char *outname) {
 
 ////////////////////////////////////////////////////////////////////////////////
 int main (int argc, char *argv[]) {
-  /* complevel = 9; */
-  /* no_zlib = 0; */
+  complevel = 4;
 
-  if (no_zlib) iflg |= ZMBV_INIT_FLAG_NOZLIB;
+#ifdef ZMBV_USE_MINIZ
+  iflg |= ZMBV_INIT_FLAG_NOZLIB;
+#endif
   printf("using compression level %d\n", complevel);
 
   max_frames_count = VIDEO_NUM_FRAMES;
